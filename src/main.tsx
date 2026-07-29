f41f162709edc3c3be87fde33bd3af4a0edbcd86
Tomas Juan Raspo <121737284+tomiraspo@users.noreply.github.com>
2026-07-29 14:38:06 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
+import "./i18n.ts"; // <-- Agregas esta línea aquí
 import App from "./App.tsx";
 import "./index.css";
 
